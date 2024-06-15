a795d043560be0c396780a6ece9091b58fca9ffa
Williams439 <andrewpwilliams1776@gmail.com>
2024-06-15 14:43:18 -0400

    parkinglotlist

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
+import { useState } from 'react';
+import {nanoid} from 'nanoid';
 import './App.css';
 
 import ParkingLotForm from "./Components/ParkingLotForm/ParkingLotForm";
 import ParkingLotList from './Components/ParkingLotList/ParkingLotList';
 
 export default function App() {
-    return (
-      <div className="App">
-        <header>
-          <h1>Browser Parking Lot</h1>
-          <p>Send most of your browser tabs into retirement.</p>
-        </header>
-        <main>
-          <ParkingLotForm />
-          <ParkingLotList />
-        </main>
-      </div>
-    );
-  }
 
+  let [parkingLotItems, setParkingLotItems] = useState([
+  {
+    id: nanoid(),  
+    date: "2024-06-01",
+    priority: "Low", 
+    link: "https://google.com/",
+    description: "Ultimate source of truth.",
+  },
+  {
+    id: nanoid(),  
+    date: "2024-05-31",
+    priority: "Medium", 
+    link: "https://react.dev/",
+    description: "React documentation and tutorial",
+  },
+  ]);
+  
+  return (
+    <div className="App">
+      <header>
+        <h1>Browser Parking Lot</h1>
+        <p>Send most of your browser tabs into retirement.</p>
+      </header>
+      <main>
+        <ParkingLotForm />
+        <ParkingLotList parkingLotItems={parkingLotItems}/>
+      </main>
+    </div>
+  );
+}
