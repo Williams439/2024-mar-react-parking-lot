acf51e6883b74dd1f47e653754ff9bae281af72d
Williams439 <andrewpwilliams1776@gmail.com>
2024-06-15 19:02:40 -0400

    delete feature

--- src/App.js
+++ src/App.js
@@ -37,6 +37,11 @@ export default function App() {
     ]);
   }
   
+  function deleteItem(id) {
+    setParkingLotItems((oldItems) =>
+        oldItems.filter(item => item.id !== id));
+  }
+
   return (
     <div className="App">
       <header>
@@ -45,7 +50,9 @@ export default function App() {
       </header>
       <main>
         <ParkingLotForm addItem={addItem} />
-        <ParkingLotList parkingLotItems={parkingLotItems}/>
+        <ParkingLotList 
+          parkingLotItems={parkingLotItems}
+          deleteItem={deleteItem}/>
       </main>
     </div>
   );
